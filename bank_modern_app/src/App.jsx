8cf096e1bd66d2677558536d8bd23cf1670a15c1
gilkag2 <gilkag2@gmail.com>
2022-10-18 20:07:53 +0300

    done with structure and layout

--- bank_modern_app/src/App.jsx
+++ bank_modern_app/src/App.jsx
@@ -1,29 +1,32 @@
 import React from 'react';
 import styles from './style';
+import {
+  Navbar, Billing, CardDeal, Business, Clients, CTA, Stats, Footer, Testimonials, Hero,
+} from './components';
 
 const App = () => (
   <div className='bg-primary w-full overflow-hidden'>4
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
 
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Hero
+        <Hero />
       </div>
     </div>
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        stats
-        business
-        billing
-        cardDeal
-        testimonials
-        clients
-        cta
-        footer
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
