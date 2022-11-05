826f2352ab7dc8140b5acf1504640c4e6d4582da
gilkag2 <gilkag2@gmail.com>
2022-11-05 16:18:11 +0200

    done project

--- bank_modern_app/src/components/Business.jsx
+++ bank_modern_app/src/components/Business.jsx
@@ -2,8 +2,8 @@ import { features } from "../constants";
 import styles, { layout } from "../style";
 import Button from './Button';
 
-const FeatureCard = ({ key, icon, title, content, index }) => (
-  <div key={key} className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
+const FeatureCard = ({ icon, title, content, index }) => (
+  <div className={`flex flex-row p-6 rounded-[20px] ${index !== features.length - 1 ? 'mb-6' : 'mb-0'} feature-card`}>
     <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
     </div>
